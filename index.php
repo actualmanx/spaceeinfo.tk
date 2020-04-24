ea08bc07cb189aae5a0da677357b1f822a0f0619
actualmanx <kieranhill@hotmail.co.uk>
2020-04-24 08:28:30 +0100

    First Upload
    Backup Of Site

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Space Engineers Info</title>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="stylesheet" href="styles.css">
+	<meta charset="utf-8">
+</head>
+<body>
+<header>
+	  
+</header>
+		
+
+		<main>
+		<section class="bg-1">
+			<table height="600" border="1" align="left">
+				<tbody>
+					<tr>
+						<th width="65" height="32" align="center" valign="middle" scope="col">Name</th>
+						<th width="47" align="center" valign="middle" scope="col">Entity Name</th>
+						<th width="43" align="center" valign="middle" scope="col">Static Grids</th>
+						<th width="45" align="center" valign="middle" scope="col">Large Grids</th>
+						<th width="43" align="center" valign="middle" scope="col">Small Grids</th>
+						<th width="76" align="center" valign="middle" scope="col">Coord's X</th>
+						<th width="68" align="center" valign="middle" scope="col">Y</th>
+						<th width="76" align="center" valign="middle" scope="col">Z</th>
+						<th width="76" align="center" valign="middle" scope="col">Zero G Cord's X</th>
+						<th width="68" align="center" valign="middle" scope="col">Y</th>
+						<th width="76" align="center" valign="middle" scope="col">Z</th>
+						<th width="166" align="center" valign="middle" scope="col">Game Readable GPS Coord's</th>
+					</tr>
+					<tr>
+						<th height="32" align="center" valign="middle" scope="col">Earth Base</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">945</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">51811.63</td>
+         <td align="center" valign="middle">-30481.93</td>
+         <td align="center" valign="middle">10634.06</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle"><button class="button-earth">Earth Base</button></td>
+                  </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Mars Base</th>
+         <td align="center" valign="middle">Mars Base</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">1033965.22</td>
+         <td align="center" valign="middle">136176.13</td>
+         <td align="center" valign="middle">1692127.11</td>
+         <td align="center" valign="middle">1035906.14</td>
+         <td align="center" valign="middle">139660.68</td>
+         <td align="center" valign="middle">1733134.84</td>
+         <td align="center" valign="middle"><button class="button-mars">Mars Base</button></td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Triton Base</th>
+         <td align="center" valign="middle">Triton Base</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">868974.99</td>
+         <td align="center" valign="middle">370.83</td>
+         <td align="center" valign="middle">1813769.31</td>
+         <td align="center" valign="middle">864381.14</td>
+         <td align="center" valign="middle">55691.05</td>
+         <td align="center" valign="middle">1810632.41</td>
+         <td align="center" valign="middle"><button class="button-triton">Triton Base</button></td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Moon Base</th>
+         <td align="center" valign="middle">Moon Base</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">21941.5</td>
+         <td align="center" valign="middle">142406.21</td>
+         <td align="center" valign="middle">-108952.58</td>
+         <td align="center" valign="middle">24114.7</td>
+         <td align="center" valign="middle">144453.91</td>
+         <td align="center" valign="middle">-106829.44</td>
+         <td align="center" valign="middle"><button class="button-moon">Moon Base</button></td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Titan Base</th>
+         <td align="center" valign="middle">Titan Base</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">110976.22</td>
+         <td align="center" valign="middle">171553.27</td>
+         <td align="center" valign="middle">5772216.97</td>
+         <td align="center" valign="middle">88370.12</td>
+         <td align="center" valign="middle">207306.27</td>
+         <td align="center" valign="middle">5791466.87</td>
+         <td align="center" valign="middle"><button class="button-titan">Titan Base</button></td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Alien Base</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle"><button class="button-alien">Alien Base</button></td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Europa Base</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">5878</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">921833.48</td>
+         <td align="center" valign="middle">21950.5</td>
+         <td align="center" valign="middle">1622103.39</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle"><button class="button-europa">Europa Base</button></td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Ice Rig</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">299</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">Unknown</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">9163</td>
+         <td align="center" valign="middle">880005.77</td>
+         <td align="center" valign="middle">52223.58</td>
+         <td align="center" valign="middle">1776446.44</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">&nbsp;</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+       <tr>
+         <th height="32" align="center" valign="middle" scope="row">&nbsp;</th>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+       <tr>
+         <th height="32" scope="row">&nbsp;</th>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+       <tr>
+         <th height="32" scope="row">&nbsp;</th>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+       <tr>
+         <th height="32" scope="row">&nbsp;</th>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td align="center" valign="middle">&nbsp;</td>
+         </tr>
+		    </tbody>
+			</table>
+		  </section>
+	</main>
+	
+	
+		<footer>
+  Created By Dilligaf.
+		</footer>
+	
+	
+	
+	
+	<script>
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a
+  // flash, so some of these are just precautions. However in
+  // Internet Explorer the element is visible whilst the popup
+  // box asking the user for permission for the web page to
+  // copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+}
+
+
+var copyEarthBtn = document.querySelector('.button-earth'),
+	copyMarsBtn = document.querySelector('.button-mars'),
+	copyTritonBtn = document.querySelector('.button-triton'),
+	copyMoonBtn = document.querySelector('.button-moon'),
+	copyTitanBtn = document.querySelector('.button-titan'),
+	copyAlienBtn = document.querySelector('.button-alien'),
+	copyEuropaBtn = document.querySelector('.button-europa');
+
+copyEarthBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('GPS:Earth Base Tony:51811.63:-30481.93:10634.06:');
+});
+copyMarsBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('GPS:Mars Base:1033965.22:136176.16:1692127.11:');
+});
+copyTritonBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('GPS:Triton Base:865974.99:370.83:1813769.31:');
+});
+copyMoonBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('GPS:Moon Base:21941.5:142406.21:-108952.58:');
+});
+copyTitanBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('GPS:Titan Base:110976.22:171553.27:5772216.97:');
+});
+copyAlienBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('Alien Base Place Holder');
+});
+copyEuropaBtn.addEventListener('click', function(event) {
+  copyTextToClipboard('GPS:Europa Moon Base:921833.48:21950.5:1622103.39:');
+});
+
+</script>
+</body>
+</html>
